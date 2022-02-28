6463177026b48498a7b70e0f4c55d818b397eeae
jay-tux <sys.jonas@yahoo.com>
2022-02-28 20:45:33 +0100

    feat: basic backend set up

---
+++ backend/index.ts
@@ -0,0 +1,19 @@
+// Import the express in typescript file
+import express from 'express';
+ 
+// Initialize the express engine
+const app: express.Application = express();
+ 
+// Take a port 3000 for running server.
+const port: number = 3000;
+ 
+// Handling '/' Request
+app.get('/', (_req, _res) => {
+    _res.send("TypeScript With Expresss");
+});
+ 
+// Server setup
+app.listen(port, () => {
+    console.log(`TypeScript with Express
+         http://localhost:${port}/`);
+});
