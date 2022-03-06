1cf9c939f460e25cf98139a4211799ad9f31459d
jay-tux <sys.jonas@yahoo.com>
2022-03-06 12:10:24 +0100

    feat: ~64% test coverage for /backend/utility.ts

--- backend/tests/utility.test.ts
+++ backend/tests/utility.test.ts
@@ -5,6 +5,10 @@ import * as config from '../config.json';
 import {ApiError} from '../types';
 import * as util from '../utility';
 
+interface Req {
+  url: string, verb: string
+}
+
 function genPromises(): Promise<any>[] {
   const data: any[] = [
     6, "abc", {"key" : "value"},
@@ -16,6 +20,13 @@ function genPromises(): Promise<any>[] {
         return res;
       }));
   return promises;
+}
+
+function obtainResponse() {
+  const {res} = getMockRes();
+  const statSpy = jest.spyOn(res, 'status');
+  const sendSpy = jest.spyOn(res, 'send');
+  return {res, statSpy, sendSpy};
 }
 
 test("utility.errors.cook* work as expected", () => {
@@ -45,7 +56,7 @@ test("utility.errors.cook* work as expected", () => {
           expect(util.errors.cookNonExistent(val.url)).toStrictEqual(val.err));
 
   // invalid verb
-  const verbs: {verb: string, url: string}[] = [
+  const verbs: Req[] = [
     {verb : "GET", url : "/"}, {verb : "POST", url : "/students/all"},
     {verb : "DELETE", url : "/coach/all"}, {verb : "PUT", url : "/login"},
     {verb : "HEAD", url : "/admin/all"},
@@ -90,22 +101,127 @@ test("utility.debug logs their data", () => {
 });
 
 test("utility.reply writes to a response", () => {
-  const {res} = getMockRes();
-
   const data: {status: number, data: any}[] = [
     {status : 200, data : "success!"}, {status : 405, data : "invalid"},
     {status : 200, data : {using : "some", kind : {of : "data"}}}
   ];
 
-  const writeSpy = jest.spyOn(res, 'send');
-  const statusSpy = jest.spyOn(res, 'status');
+  const {res, statSpy, sendSpy} = obtainResponse();
 
   const promises: Promise<void>[] =
       data.map(data => util.reply(res, data.status, data.data).then(() => {
-        expect(writeSpy).toBeCalledWith(data.data);
-        expect(statusSpy).toBeCalledWith(data.status);
+        expect(sendSpy).toBeCalledWith(data.data);
+        expect(statSpy).toBeCalledWith(data.status);
       }));
   return Promise.all(promises);
 });
 
-// test("utility.replyError writes to a response")
+test("utility.replyError writes to a response", () => {
+  // load api errors
+  const someUrls: string[] = [ "/", "/logout", "/signup" ];
+  const someVerbs: Req[] = [
+    {verb : "PUT", url : "/"}, {verb : "OPTIONS", url : "/login"},
+    {verb : "DELETE", url : "/student/all"}
+  ];
+  const someReqs: express.Request[] = someVerbs.map(req => {
+    var request: express.Request = getMockReq();
+    request.method = req.verb;
+    request.url = req.url;
+    return request;
+  });
+  const someMimes: string[] =
+      [ 'text/html', 'audio/aac', 'image/bmp', 'text/css', 'video/mp4' ];
+
+  var errors: ApiError[] = [
+    util.errors.cookInvalidID(), util.errors.cookArgumentError(),
+    util.errors.cookUnauthenticated(), util.errors.cookInsufficientRights(),
+    util.errors.cookServerError()
+  ];
+  someUrls.forEach(url => errors.push(util.errors.cookNonExistent(url)));
+  someReqs.forEach(req => errors.push(util.errors.cookInvalidVerb(req)));
+  someMimes.forEach(mime => errors.push(util.errors.cookNonJSON(mime)));
+
+  // run tests
+  errors.forEach(err => {
+    const {res, statSpy, sendSpy} = obtainResponse();
+    const datafield: any = {success : false, reason : err.reason};
+    util.replyError(res, err).then(() => {
+      expect(statSpy).toBeCalledWith(err.http);
+      expect(sendSpy).toBeCalledWith(datafield);
+    });
+  });
+});
+
+test("utility.replySuccess writes to a response", () => {
+  const successes: any[] = [];
+
+  successes.forEach(succ => {
+    const {res, statSpy, sendSpy} = obtainResponse();
+    util.replySuccess(res, succ).then(() => {
+      succ.success = true;
+      expect(statSpy).toBeCalledWith(200);
+      expect(sendSpy).toBeCalledWith(succ);
+    });
+  });
+});
+
+test("utility.addInvalidVerbs adds verbs", () => {
+  var router: express.Router = express.Router();
+  const routerSpy = jest.spyOn(router, 'all');
+  util.addInvalidVerbs(router, '/');
+  // we seem unable to trigger the route with jest?
+  // please someone check this?
+  expect(routerSpy).toHaveBeenCalledTimes(1);
+});
+
+test("utility.logRequest logs request and passes control", () => {
+  const writeSpy = jest.spyOn(console, "log");
+  const request: express.Request = getMockReq();
+  const callback = jest.fn(
+      () => { expect(writeSpy).toHaveBeenCalledWith("GET /student/all"); });
+  request.method = "GET";
+  request.url = "/student/all";
+  util.logRequest(request, callback);
+  expect(callback).toHaveReturnedTimes(1);
+});
+
+test("utility.respOrErrorNoReinject sends successes", () => {
+  // positive case
+  const {res, statSpy, sendSpy} = obtainResponse();
+  var obj:
+      any = {data : {"some" : "data"}, "sessionkey" : "updated-session-key"};
+  util.respOrErrorNoReinject(res, Promise.resolve(obj)).then(() => {
+    obj.success = true;
+    expect(statSpy).toHaveBeenCalledWith(200);
+    expect(sendSpy).toHaveBeenCalledWith(obj);
+  });
+});
+
+test("utility.respOrErrorNoReinject sends api errors", () => {
+  // api error case
+  const {res, statSpy, sendSpy} = obtainResponse();
+  var err: ApiError = util.errors.cookArgumentError();
+  var errObj: any = {success : false, reason : err.reason};
+  util.respOrErrorNoReinject(res, Promise.reject(err)).then(() => {
+    expect(statSpy).toHaveBeenCalledWith(err.http);
+    expect(sendSpy).toHaveBeenCalledWith(errObj);
+  });
+});
+
+test("utility.respOrErrorNoReinject sends generic errors as server errors",
+     () => {
+       const {res, statSpy, sendSpy} = obtainResponse();
+       const writeSpy = jest.spyOn(console, "log");
+       const error = {msg : "some", error : {"generic" : true}};
+       const send = {
+         success : false,
+         reason : config.apiErrors.serverError.reason
+       };
+       util.respOrErrorNoReinject(res, Promise.reject(error)).then(() => {
+         expect(statSpy).toHaveBeenCalledWith(
+             config.apiErrors.serverError.http);
+         expect(sendSpy).toHaveBeenCalledWith(send);
+         expect(writeSpy).toHaveBeenCalledWith("UNCAUGHT ERROR " +
+                                               JSON.stringify(error));
+       });
+     });
