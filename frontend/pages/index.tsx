85a1e2664b2ab5ee9e4b9ba6306c24da954baecd
Mouwrice <mauricevanwassenhove@gmail.com>
2022-03-13 15:16:00 +0100

    feat: add UnderConstruction component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,10 @@
 import type {NextPage} from 'next'
-import Image from 'next/image'
-import UnderConstructionImage from '../public/images/under_construction_trans.png'
-import styles from '../styles/index.module.css'
+import {UnderConstruction} from "../components/UnderConstruction/UnderConstruction";
 
 const Home: NextPage = () => {
     return (
-        <div className={styles.container}>
-            <Image
-                src={UnderConstructionImage}
-                layout="responsive"
-                alt="Site is still under construction"
-            />
-        </div>)
+        <UnderConstruction/>
+    )
 }
 
 export default Home;
---
+++ frontend/pages/login.tsx
@@ -0,0 +1,15 @@
+import type {NextPage} from 'next'
+import styles from '../styles/login.module.css'
+import {UnderConstruction} from "../components/UnderConstruction/UnderConstruction";
+
+const Login: NextPage = () => {
+    return (
+        <div>
+            <div className={styles.header}>
+
+            </div>
+            <UnderConstruction/>
+        </div>)
+}
+
+export default Login;
