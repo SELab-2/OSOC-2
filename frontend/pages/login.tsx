32034d7c701472a5c30ca554b8051b04db6360f3
Mouwrice <mauricevanwassenhove@gmail.com>
2022-03-13 16:09:35 +0100

    feat: setup header for login page

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,13 +1,24 @@
 import type {NextPage} from 'next'
 import styles from '../styles/login.module.css'
+import Image from "next/image"
 import {UnderConstruction} from "../components/UnderConstruction/UnderConstruction";
+import LogoOsocColor from "../public/images/logo-osoc-color.svg"
 
 const Login: NextPage = () => {
     return (
         <div>
-            <div className={styles.header}>
-
-            </div>
+            <header className={styles.header}>
+                <div>
+                    <div className={styles.imagecontainer}>
+                        <Image
+                            src={LogoOsocColor}
+                            layout="intrinsic"
+                            alt="OSOC Logo"
+                        />
+                    </div>
+                    <h1>Selections</h1>
+                </div>
+            </header>
             <UnderConstruction/>
         </div>)
 }
