d0dbbef611a2ff91569e162936a2ca50c1373785
jay-tux <sys.jonas@yahoo.com>
2022-03-17 18:10:02 +0100

    fix: refactored backend/routes/coach.ts to use parsers

--- backend/routes/coach.ts
+++ backend/routes/coach.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import * as rq from '../request';
 import {InternalTypes, Responses} from '../types';
 import * as util from '../utility';
 
@@ -11,12 +12,12 @@ import * as util from '../utility';
  */
 async function listCoaches(req: express.Request):
     Promise<Responses.IdNameList> {
-    return util.checkSessionKey(req).then((_: express.Request) => {
-        let coaches: InternalTypes.IdName[] = [];
-        // TODO list all coaches
-
-        return Promise.resolve({data : coaches, sessionkey : ''});
-    });
+  return rq.parseCoachAllRequest(req)
+      .then(parsed => util.checkSessionKey(parsed))
+      .then(parsed => {
+        // FETCHING LOGIC
+        return Promise.resolve({data : [], sessionkey : parsed.sessionkey});
+      });
 }
 
 /**
@@ -25,27 +26,23 @@ async function listCoaches(req: express.Request):
  *  @returns See the API documentation. Successes are passed using
  * `Promise.resolve`, failures using `Promise.reject`.
  */
-async function getCoach(req: express.Request):
-    Promise<Responses.Coach>{return util.checkSessionKey(req).then(
-    async (_) => {// check valid id
-        // if invalid: return Promise.reject(util.cookInvalidID())
-        // if valid: get coach data etc etc
-        return Promise.resolve({
-            data : {id : '', name : '', email : '', projectid : ''},
-            sessionkey : ''
-        })})}
+async function getCoach(req: express.Request): Promise<Responses.Coach> {
+  return rq.parseSingleCoachRequest(req)
+      .then(parsed => util.checkSessionKey(parsed))
+      .then(parsed => {
+        // FETCHING LOGIC
+        return Promise.resolve({data : {}, sessionkey : parsed.sessionkey});
+      });
+}
 
 async function modCoach(req: express.Request):
     Promise<Responses.Keyed<string>> {
-    return util.checkSessionKey(req).then(async (_) => {
-        // check valid id
-        // if invalid: return Promise.reject(util.cookInvalidID());
-        // if valid: modify coach data
-        return Promise.resolve({
-            data : '',
-            sessionkey : ''
-        });
-    });
+  return rq.parseUpdateCoachRequest(req)
+      .then(parsed => util.checkSessionKey(parsed))
+      .then(parsed => {
+        // UPDATING LOGIC
+        return Promise.resolve({data : '', sessionkey : parsed.sessionkey});
+      });
 }
 
 /**
@@ -54,14 +51,13 @@ async function modCoach(req: express.Request):
  *  @returns See the API documentation. Successes are passed using
  * `Promise.resolve`, failures using `Promise.reject`.
  */
-async function deleteCoach(req: express.Request):
-    Promise<Responses.Key> {
-    return util.checkSessionKey(req).then(async (_) => {
-        // check valid id
-        // if invalid: return Promise.reject(util.cookInvalidID());
-        // if valid: delete coach data
-        return Promise.resolve({sessionkey : ''});
-    });
+async function deleteCoach(req: express.Request): Promise<Responses.Key> {
+  return rq.parseDeleteCoachRequest(req)
+      .then(parsed => util.isAdmin(parsed))
+      .then(parsed => {
+        // REMOVING LOGIC
+        return Promise.resolve({sessionkey : parsed.sessionkey});
+      });
 }
 
 /**
@@ -71,28 +67,26 @@ async function deleteCoach(req: express.Request):
  * `Promise.resolve`, failures using `Promise.reject`.
  */
 async function getCoachRequests(req: express.Request):
-    Promise<Responses.Keyed<InternalTypes.CoachRequest[]>>{return util.checkSessionKey(req).then(
-    async (_) => {
-        let coachRequests: InternalTypes.CoachRequest[] = [];
-        return Promise.resolve({
-            data : coachRequests,
-            sessionkey : ''
-        })})}
+    Promise<Responses.Keyed<InternalTypes.CoachRequest[]>> {
+  return rq.parseGetCoachRequestRequest(req)
+      .then(parsed => util.isAdmin(parsed))
+      .then(parsed => {
+        // FETCHING LOGIC
+        return Promise.resolve({data : [], sessionkey : parsed.sessionkey});
+      });
+}
 
 /**
- *  Attempts to create a new student in the system.
+ *  Attempts to create a new user in the system.
  *  @param req The Express.js request to extract all required data from.
  *  @returns See the API documentation. Successes are passed using
  * `Promise.resolve`, failures using `Promise.reject`.
  */
-async function createCoachRequest(req: express.Request):
-    Promise<Responses.Keyed<string>> {
-    return util.checkSessionKey(req).then((_: express.Request) => {
-        let id: string = '';
-        // TODO do insertion logic
-
-        return Promise.resolve({data : id, sessionkey : ''});
-    });
+async function createCoachRequest(req: express.Request): Promise<string> {
+  return rq.parseRequestCoachRequest(req).then(parsed => {
+    // INSERTION LOGIC -> NEEDS FIXING
+    return Promise.resolve(parsed.firstName);
+  });
 }
 
 /**
@@ -102,15 +96,17 @@ async function createCoachRequest(req: express.Request):
  * `Promise.resolve`, failures using `Promise.reject`.
  */
 async function getCoachRequest(req: express.Request):
-    Promise<Responses.Keyed<InternalTypes.CoachRequest>>{return util.checkSessionKey(req).then(
-    async (_) => {
-        // check valid id
-        // if invalid: return Promise.reject(util.cookInvalidID());
-        // if valid: get request details
+    Promise<Responses.Keyed<InternalTypes.CoachRequest>> {
+  return rq.parseGetCoachRequestRequest(req)
+      .then(parsed => util.isAdmin(parsed))
+      .then(parsed => {
+        // FETCHING LOGIC
         return Promise.resolve({
-            data : {id : '', name : '', email : ''},
-            sessionkey : ''
-        })})}
+          data : {id : '', name : '', email : ''},
+          sessionkey : parsed.sessionkey
+        });
+      });
+}
 
 /**
  *  Attempts to accept a request for becoming a coach.
@@ -119,15 +115,15 @@ async function getCoachRequest(req: express.Request):
  * `Promise.resolve`, failures using `Promise.reject`.
  */
 async function createCoachAcceptance(req: express.Request):
-    Promise<Responses.Keyed<InternalTypes.IdName>>{return util.checkSessionKey(req).then(
-    async (_) => {
-        // check valid id
-        // if invalid: return Promise.reject(util.cookInvalidID());
-        // if valid: create coach acceptance request
-        return Promise.resolve({
-            data : {id : '', name : ''},
-            sessionkey : ''
-        })})}
+    Promise<Responses.Keyed<InternalTypes.IdName>> {
+  return rq.parseAcceptNewCoachRequest(req)
+      .then(parsed => util.isAdmin(parsed))
+      .then(parsed => {
+        // UPDATING LOGIC
+        return Promise.resolve(
+            {data : {id : '', name : ''}, sessionkey : parsed.sessionkey});
+      });
+}
 
 /**
  *  Attempts to deny a request for becoming a coach.
@@ -136,14 +132,14 @@ async function createCoachAcceptance(req: express.Request):
  * `Promise.resolve`, failures using `Promise.reject`.
  */
 async function deleteCoachRequest(req: express.Request):
-    Promise<Responses.Key>{return util.checkSessionKey(req).then(
-    async (_) => {
-        // check valid id
-        // if invalid: return Promise.reject(util.cookInvalidID());
-        // if valid: deny request for becoming a coach
-        return Promise.resolve({
-            sessionkey : ''
-        })})}
+    Promise<Responses.Key> {
+  return rq.parseDenyNewCoachRequest(req)
+      .then(parsed => util.isAdmin(parsed))
+      .then(parsed => {
+        // UPDATING LOGIC
+        return Promise.resolve({sessionkey : parsed.sessionkey});
+      });
+}
 
 /**
  *  Gets the router for all `/coaches/` related endpoints.
@@ -151,25 +147,29 @@ async function deleteCoachRequest(req: express.Request):
  * endpoints.
  */
 export function getRouter(): express.Router {
-    let router: express.Router = express.Router();
+  let router: express.Router = express.Router();
 
-    router.get("/", (_, res) => util.redirect(res, "/coach/all"));
-    util.route(router, "get", "/all", listCoaches);
-    util.route(router, "get", "/:id", getCoach);
+  router.get("/", (_, res) => util.redirect(res, "/coach/all"));
+  util.route(router, "get", "/all", listCoaches);
+  util.route(router, "get", "/:id", getCoach);
 
-    util.route(router, "post", "/:id", modCoach);
-    router.delete('/:id',
-        (req, res) => util.respOrErrorNoReinject(res, deleteCoach(req)));
+  util.route(router, "post", "/:id", modCoach);
+  router.delete('/:id', (req, res) =>
+                            util.respOrErrorNoReinject(res, deleteCoach(req)));
 
-    util.route(router, "get", "/request", getCoachRequests);
-    util.route(router, "post", "/request", createCoachRequest);
-    util.route(router, "get", "/request/:id", getCoachRequest);
+  util.route(router, "get", "/request", getCoachRequests);
+  // signup is not a keyed request - doesn't match the callback type.
+  // util.route(router, "post", "/request", createCoachRequest);
+  router.post('/request', (req, res) => util.respOrErrorNoReinject(
+                              res, createCoachRequest(req)));
+  util.route(router, "get", "/request/:id", getCoachRequest);
 
-    util.route(router, "post", "/request/:id", createCoachAcceptance);
-    router.delete('/request/:id',
-        (req, res) => util.respOrErrorNoReinject(res, deleteCoachRequest(req)));
+  util.route(router, "post", "/request/:id", createCoachAcceptance);
+  router.delete('/request/:id', (req, res) => util.respOrErrorNoReinject(
+                                    res, deleteCoachRequest(req)));
 
-    util.addAllInvalidVerbs(router, [ "/", "/all", "/:id", "/request", "/request/:id" ]);
+  util.addAllInvalidVerbs(router,
+                          [ "/", "/all", "/:id", "/request", "/request/:id" ]);
 
-    return router;
+  return router;
 }
