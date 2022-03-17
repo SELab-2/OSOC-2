9c7b89bbea38f555b60df60fa8583f634a426c7d
KoenDesplenter <koen.desplenter@telenet.be>
2022-03-17 17:59:12 +0100

    fix: fixed function names

--- backend/orm_functions/job_application_skill.ts
+++ backend/orm_functions/job_application_skill.ts
@@ -5,7 +5,7 @@ import {CreateJobApplicationSkill, UpdateJobApplicationSkill} from './orm_types'
  * 
  * @param jobApplicationSkill: job application skill object with the needed information
  */
- export async function createStudent(jobApplicationSkill: CreateJobApplicationSkill) {
+ export async function createJobApplicationSkill(jobApplicationSkill: CreateJobApplicationSkill) {
     return prisma.job_application_skill.create({
         data: {
             job_application_id: jobApplicationSkill.JobApplicationId,
@@ -57,7 +57,7 @@ import {CreateJobApplicationSkill, UpdateJobApplicationSkill} from './orm_types'
  * @param jobApplicationSkill: UpdateStudent object with the values that need to be updated
  * @returns the updated entry in the database 
  */
- export async function updateStudent(jobApplicationSkill: UpdateJobApplicationSkill) {
+ export async function updateJobApplicationSkill(jobApplicationSkill: UpdateJobApplicationSkill) {
     return await prisma.job_application_skill.update({
         where: {
             job_application_skill_id: jobApplicationSkill.JobApplicationSkillId,
@@ -78,7 +78,7 @@ import {CreateJobApplicationSkill, UpdateJobApplicationSkill} from './orm_types'
  * @param jobApplicationSkillId the job application skill we are deleting from the table
  * @returns the job application, this info can be used to further remove the job application
  */
- export async function deleteStudent(jobApplicationSkillId: number) {
+ export async function deleteJobApplicationSkill(jobApplicationSkillId: number) {
     return await prisma.job_application_skill.delete({
         where: {
             job_application_skill_id: jobApplicationSkillId,
