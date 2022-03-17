ca4b59155c21460ca308ec7a4ed0eea3d7c8bb9f
KoenDesplenter <koen.desplenter@telenet.be>
2022-03-17 09:50:25 +0100

    fix: added docs

--- backend/orm_functions/project_role.ts
+++ backend/orm_functions/project_role.ts
@@ -1,27 +1,41 @@
 import prisma from '../prisma/prisma'
+import {CreateProjectRole, UpdateProjectRole} from './orm_types';
 
-
-// Create Project Role
-export async function createProjectRole(projectId : number, roleId : number, positions : number){
+/**
+ * 
+ * @param projectRole: project role object with the needed information
+ */
+export async function createProjectRole(projectRole: CreateProjectRole){
     const result = await prisma.project_role.create({
         data: {
-            project_id : projectId,
-            role_id : roleId,
-            positions : positions
+            project_id: projectRole.projectId,
+            role_id: projectRole.roleId,
+            positions: projectRole.positions
         },
     });
     return result;
 }
 
-// Get all Project Roles for a certain Project
-export async function getProjectRolesByProject(projectId : number) {
+/**
+ * 
+ * @param projectId: this is the id of the project for which we want all the roles
+ * @returns: all the project role objects for that project
+ */
+export async function getProjectRolesByProject(projectId: number) {
     const result = prisma.project_role.findMany({
-        where: { project_id : projectId},
+        where: { 
+            project_id: projectId
+        },
     });
     return result;
 }
 
-// Get number of Project Roles for a certain Project and Project role
+/**
+ * 
+ * @param projectId: this is the id of the project for which we want the number of positions
+ * @param projectRoleId: this is the id of the projectRole for which we want the number of positions 
+ * @returns: returns the number of positions for the projectrole of that project
+ */
 export async function getNumberOfRolesByProjectAndRole(projectId : number, projectRoleId : number) {
     const result = prisma.project_role.findMany({
         where: { 
@@ -39,34 +53,49 @@ export async function getNumberOfRolesByProjectAndRole(projectId : number, proje
 }
 
 // Get all Project Role Names for a certain Project
-export async function getProjectRoleNamesByProject(projectId : number) {
+/**
+ * 
+ * @param projectId: this is the id of the project for which we want all the roles
+ * @returns: returns all the projectroles object togheter with the role objects for that project
+ */
+export async function getProjectRoleNamesByProject(projectId: number) {
     const result = prisma.project_role.findMany({
-        where: { project_id : projectId},
-        include : {
-            role : true
+        where: { 
+            project_id: projectId
+        },
+        include: {
+            role: true
         }
     });
     return result;
 }
 
-// Update ProjectRole
-export async function updateProject(projectRoleId : number, positions : number){
+/**
+ * 
+ * @param projectRole: UpdateProject object with the values that need to be updated
+ * @returns the updated entry in the database 
+ */
+export async function updateProject(projectRole: UpdateProjectRole){
         const result = await prisma.project_role.update({
-        where : {
-            project_role_id : projectRoleId
+        where: {
+            project_role_id: projectRole.projectRoleId
         },
         data: {
-            positions : positions
+            positions: projectRole.positions
         },
     });
     return result;
 }
 
-// Delete ProjectRole
-export async function deleteProject(projectRoleId : number){
+/**
+ * 
+ * @param projectRoleId the projectRole we are deleting from the project role-table
+ * @returns TODO: what does this return?
+ */
+export async function deleteProject(projectRoleId: number){
     const result = await prisma.project_role.delete({
-        where : {
-            project_role_id : projectRoleId
+        where: {
+            project_role_id: projectRoleId
         }
     });
     return result;
