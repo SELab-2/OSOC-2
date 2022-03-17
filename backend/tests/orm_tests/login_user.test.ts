39286c8eb0a44890113d134c465280efc85c4068
Bram Devlaminck <bram.devlaminck@gmail.com>
2022-03-17 22:01:16 +0100

    test: also fix cause a test was neededin login_user.test.ts to pass the github actions

--- backend/tests/orm_tests/login_user.test.ts
+++ backend/tests/orm_tests/login_user.test.ts
@@ -1,12 +1,31 @@
-// example response object that might be handy to use once new tests for this file get written
+import {account_status_enum} from "@prisma/client";
+import {prismaMock} from "./singleton";
+import {createLoginUser} from "../../orm_functions/login_user";
+import {CreateLoginUser} from "../../orm_functions/orm_types";
 
-// const response = {
-//   session_id : "50",
-//   login_user_id : 1,
-//   person_id : 0,
-//   password : "password",
-//   is_admin : false,
-//   is_coach : false,
-//   session_keys : [ "key1", "key2" ],
-//   account_status : account_status_enum.DISABLED
-//}
+const response = {
+  session_id : "50",
+  login_user_id : 1,
+  person_id : 0,
+  password : "password",
+  is_admin : false,
+  is_coach : false,
+  session_keys : [ "key1", "key2" ],
+  account_status : account_status_enum.DISABLED
+}
+
+test("should create a login_user", async () => {
+
+    const loginUser: CreateLoginUser = {
+        accountStatus: account_status_enum.ACTIVATED,
+        isAdmin: false,
+        isCoach: false,
+        password: "password",
+        personId: 0
+    }
+
+    prismaMock.login_user.create.mockResolvedValue(response);
+    await expect(createLoginUser(loginUser)).resolves.toEqual(response);
+});
+
+// TODO: write all the other tests for this file
