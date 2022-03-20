1dac2c8b70d09269e191b28e62a77e91938e1f33
KoenDesplenter <koen.desplenter@telenet.be>
2022-03-20 14:13:08 +0100

    feat: created tests for applied role

---
+++ backend/tests/orm_tests/applied_role.test.ts
@@ -0,0 +1,18 @@
+import {prismaMock} from "./singleton";
+import { CreateAppliedRole } from "../../orm_functions/orm_types";
+import {createAppliedRole } from "../../orm_functions/applied_role";
+
+const returnValue = {
+    role_id: 0,
+    job_application_id: 0
+}
+
+test("should create an applied role in the db with the given object, returns the new record", async () => {
+    const appliedRole: CreateAppliedRole = {
+        jobApplicationId: 0,
+        roleId: 0
+    };
+ 
+     prismaMock.applied_role.create.mockResolvedValue(returnValue)
+     await expect(createAppliedRole(appliedRole)).resolves.toEqual(returnValue);
+ });
