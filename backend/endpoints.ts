8e9e430229efd287efae19a64a341d67b6d80fa3
jay-tux <sys.jonas@yahoo.com>
2022-03-20 14:40:06 +0100

    feat: migration part one (config.json update)

--- backend/endpoints.ts
+++ backend/endpoints.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import * as login from './routes/login';
-import * as student from './routes/student';
-import * as coach from './routes/coach';
+import * as config from './config.json';
 import * as admin from './routes/admin';
-import * as project from './routes/project';
+import * as coach from './routes/coach';
 import * as form from './routes/form';
+import * as login from './routes/login';
+import * as project from './routes/project';
+import * as student from './routes/student';
 import * as util from './utility';
 
 /**
@@ -13,12 +14,14 @@ import * as util from './utility';
  *  @param app The Express.js application to attach to.
  */
 export function attach(app: express.Application): void {
-  app.use('/login', login.getRouter());
-  app.use('/student', student.getRouter());
-  app.use('/coach', coach.getRouter());
-  app.use('/admin', admin.getRouter());
-  app.use('/project', project.getRouter());
-  app.use('/form', form.getRouter);
+  config.homes.forEach(home => {
+    app.use(home + '/login', login.getRouter());
+    app.use(home + '/student', student.getRouter());
+    app.use(home + '/coach', coach.getRouter());
+    app.use(home + '/admin', admin.getRouter());
+    app.use(home + '/project', project.getRouter());
+    app.use(home + '/form', form.getRouter);
+  });
 
   app.use((req: express.Request, res: express.Response): Promise<void> =>
               util.replyError(res, util.errors.cookNonExistent(req.url)));
