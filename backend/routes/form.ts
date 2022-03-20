69cf710ec38e5d8a5aabc7d44000decfab629590
Norick Beterams <norick.beterams@ugent.be>
2022-03-20 13:18:45 +0100

    feat: create person from form

--- backend/routes/form.ts
+++ backend/routes/form.ts
@@ -1,21 +1,54 @@
-/*import express from 'express';
+import express from 'express';
 
 import * as ormP from '../orm_functions/person';
 import * as rq from '../request';
 import {Requests, Responses} from '../types';
+import * as util from "../utility";
 
+function filterQuestion(form : Requests.Form, key : string) :
+    Requests.Question {
+    return form.data.fields.filter(question => question.key == key)[0];
+}
 
-function json_to_person(form : Requests.Form):
+function checkYesAnswer(question : Requests.Question) :
+    Boolean {
+    if (question.options !== undefined) {
+        return question.options.filter(option => option.id === question.value)[0].text.toLowerCase().includes("yes");
+    }
+    return false;
+}
+
+function checkInBelgium(form : Requests.Form) :
+    Boolean {
+    return checkYesAnswer(filterQuestion(form, "question_wkNolR"));
+}
+
+function checkCanWorkJuly(form : Requests.Form) :
+    Boolean {
+    return checkYesAnswer(filterQuestion(form, "question_mKVEz8"));
+}
+
+function jsonToPerson(form : Requests.Form):
     Promise<Responses.Empty> {
+    let questionBirthName : Requests.Question = filterQuestion(form, "question_npDErJ");
+    let questionLastName : Requests.Question = filterQuestion(form, "question_319eXp");
+    let questionEmail : Requests.Question = filterQuestion(form, "question_mY46PB");
+    // TODO check email
+    let questionGender : Requests.Question = filterQuestion(form, "question_wg9laO");
+    let gender : string = "";
+    if (questionGender.options !== undefined) {
+        gender = questionGender.options.filter(option => option.id === questionGender.value)[0].text;
+    }
+
     return ormP.create_person({
-        firstname : "",
-        lastname : "",
-        email : "",
-        gender : ""
+        firstname : questionBirthName.value,
+        lastname : questionLastName.value,
+        email : questionEmail.value,
+        gender : gender
     }).then(() => {
         return Promise.resolve({});
     });
-}*/
+}
 
 /**
  *  Attempts to create a new user in the system.
@@ -23,16 +56,35 @@ function json_to_person(form : Requests.Form):
  *  @returns See the API documentation. Successes are passed using
  * `Promise.resolve`, failures using `Promise.reject`.
  */
-/*async function createFormRequest(req: express.Request):
-    Promise<Responses.Empty> {
+async function createForm(req: express.Request):
+    Promise<Responses.Keyed<string>> {
     return rq.parseFormRequest(req).then(async form => {
-        //let fields : Array<object> = form.data.fields;
-        return Promise.resolve({});
-        return json_to_person(form)
-            .then(_ => {
-                return Promise.resolve({});
+        if(!checkInBelgium(form) || !checkCanWorkJuly(form)) {
+            return Promise.reject(util.errors.cookNonJSON("Invalid json"));
+        }
+
+        return jsonToPerson(form)
+            .then(() => {
+                return Promise.resolve({data : "", sessionkey : ""});
             });
     });
-}*/
+}
+
+/**
+ *  Gets the router for all `/form/` related endpoints.
+ *  @returns An Express.js {@link express.Router} routing all `/form/`
+ * endpoints.
+ */
+export function getRouter(): express.Router {
+    let router: express.Router = express.Router();
+
+    util.route(router, "post", "/", createForm);
+
+    util.addAllInvalidVerbs(
+        router,
+        [ "/" ]);
+
+    return router;
+}
 
 
