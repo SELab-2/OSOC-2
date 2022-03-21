ca44eaf6cc207009bbf1846090d7efaf5b156c65
Bram Devlaminck <bram.devlaminck@gmail.com>
2022-03-21 20:25:10 +0100

    test: fix the first test

--- backend/tests/orm_integration/person.test.ts
+++ backend/tests/orm_integration/person.test.ts
@@ -12,7 +12,7 @@ it('should create 1 new person', async () => {
     const created_person = await createPerson(person);
     expect(created_person).toHaveProperty("github", null);
     expect(created_person).toHaveProperty("firstname", person.firstname);
-    expect(created_person).toHaveProperty("github", person.github);
+    expect(created_person).toHaveProperty("github", null);
     expect(created_person).toHaveProperty("firstname", person.firstname);
     expect(created_person).toHaveProperty("lastname", person.lastname);
     expect(created_person).toHaveProperty("email", person.email);
