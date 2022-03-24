091e2cb1f085547f714a6dabee9ad2b884bc0d96
Mouwrice <mauricevanwassenhove@gmail.com>
2022-03-24 16:30:24 +0100

    chore: cleanup authorize function

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -22,20 +22,12 @@ export default NextAuth({
                 email: { type: "text" },
                 password: { type: "password" }
             },
-            // Make call to our own api
-            async authorize(credentials, req) {
-                // Add logic here to look up the user from the credentials supplied
-                console.log(req)
-                console.log(credentials)
-
-                // TODO -- Check if users exists in the database
+            async authorize(credentials) {
 
                 let user = null;
                 if (credentials) {
                     user = {id: 1, email: credentials.email}
                 }
-
-                console.log(user)
                 return user
             }
         }),
