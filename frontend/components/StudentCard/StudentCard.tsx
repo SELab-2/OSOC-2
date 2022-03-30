69ed082fd5e4d63875ac7664adfa3685a29da93a
Jonathan Vanbrabant <jonathan.vanbrabant@ugent.be>
2022-03-30 15:59:02 +0200

    feat: created StudentCard component

---
+++ frontend/components/StudentCard/StudentCard.tsx
@@ -0,0 +1,7 @@
+export const StudentCard: React.FC = () => {
+    return (
+        <>
+            <h1>Students overview</h1>
+        </>
+    )
+}
--- frontend/pages/students.tsx
+++ frontend/pages/students.tsx
@@ -1,16 +1,12 @@
-/**
- * Students overview page
- * TODO
- */
 import {NextPage} from "next";
-import {UnderConstruction} from "../components/UnderConstruction/UnderConstruction";
 import {Header} from "../components/Header/Header";
+import {StudentCard} from "../components/StudentCard/StudentCard";
 
 const Students: NextPage = () => {
     return (
         <>
             <Header/>
-            <UnderConstruction/>
+            <StudentCard></StudentCard>
         </>
     )
 }
