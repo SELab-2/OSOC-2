97dab28ee8169428ed367d5216d44e3dee14035d
Jonathan Vanbrabant <jonathan.vanbrabant@ugent.be>
2022-03-31 15:37:21 +0200

    feat: students data inladen via props

--- frontend/pages/students.tsx
+++ frontend/pages/students.tsx
@@ -1,6 +1,18 @@
 import {NextPage} from "next";
 import {Header} from "../components/Header/Header";
 import {StudentCard} from "../components/StudentCard/StudentCard";
+import { GetStaticProps } from 'next';
+
+
+
+export const getStaticProps: GetStaticProps = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/student/all`);
+    const students = (await res.json());
+    console.log(students);
+    return {
+        props: students
+    }
+}
 
 const Students: NextPage = () => {
     return (
