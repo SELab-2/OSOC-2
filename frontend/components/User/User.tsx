2d2ea3df4094cbc5a80dfffc9586b40af554c8b3
Huan <huanyutan1999@gmail.com>
2022-04-02 21:19:11 +0200

    feat: making the user component

--- frontend/components/User/User.tsx
+++ frontend/components/User/User.tsx
@@ -1,4 +1,13 @@
 import styles from "./User.module.css";
+import AdminIcon from "../../public/images/admin_icon.png";
+import AdminIconColor from "../../public/images/admin_icon_color.png";
+import CoachIcon from "../../public/images/coach_icon.png";
+import CoachIconColor from "../../public/images/coach_icon_color.png";
+import ForbiddenIcon from "../../public/images/forbidden_icon.png";
+import ForbiddenIconColor from "../../public/images/forbidden_icon_color.png";
+import Image, {StaticImageData} from "next/image";
+import React from "react";
+
 
 export const User: React.FC = () => {
     //boilerplate for the admin/coaches route (pass admin/coach as string)
@@ -26,9 +35,43 @@ export const User: React.FC = () => {
             });
         console.log(response)
     }
+    //TODO dit moet nog afgewerkt worden
+    function button(image1: StaticImageData, image2: StaticImageData, usage: string) {
+        return (<button className={styles.button}>
+            <span> {usage}
+                <span>
+                   <Image className={styles.buttonImage}
+                          width={30}
+                          height={30}
+                          src={image1}
+                          alt={"black and white version"}
+                   />
+                    <Image className={styles.displayNone}
+                           src={image2}
+                           width={30}
+                           height={30}
+                           alt={"colored version"}
 
-    getAlluser("admin")
-    return (<div className={styles.container}>
-        <h1>Users</h1>
-    </div>)
+                    />
+                </span>
+            </span>
+        </button>)
+    }
+    //TODO dit sta hier voor tslint
+    console.log(getAlluser("admin"))
+    return (
+        <div className={styles.row}>
+            <div className={styles.column}>
+                <p>name <b>[Status pending]</b></p>
+            </div>
+            <div className={styles.column}>
+                <p>email</p>
+            </div>
+            <div className={styles.column}>
+                {button(AdminIcon, AdminIconColor, "admin")}
+                {button(CoachIcon, CoachIconColor, "coach")}
+                {button(ForbiddenIcon, ForbiddenIconColor, "admin")}
+
+            </div>
+        </div>)
 }
