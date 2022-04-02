63b468029b3b634af90fdbc5890fe869a2bd0ba5
Jonathan Vanbrabant <jonathan.vanbrabant@ugent.be>
2022-04-02 10:12:19 +0200

    fix: can't seem to get 'GET student/all' to work

--- frontend/pages/students.tsx
+++ frontend/pages/students.tsx
@@ -1,20 +1,31 @@
 import {NextPage} from "next";
 import {Header} from "../components/Header/Header";
 import {StudentCard} from "../components/StudentCard/StudentCard";
-import { GetStaticProps } from 'next';
+import SessionContext from "../contexts/sessionProvider";
+import {useContext, useEffect} from "react";
 
-
-
-export const getStaticProps: GetStaticProps = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/student/all`);
-    const students = (await res.json());
-    console.log(students);
-    return {
-        props: students
-    }
-}
 
 const Students: NextPage = () => {
+    const {sessionKey} = useContext(SessionContext);
+    //const [students, setStudents] = useState<Array<InternalTypes.Student>>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            //console.log(sessionKey);
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/student/all`, {
+                method: 'GET',
+                body: JSON.stringify({sessionkey: sessionKey}), //TODO Autherize the user
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            });
+            console.log(response)
+        }
+
+        fetchData();
+    });
+    
     return (
         <>
             <Header/>
