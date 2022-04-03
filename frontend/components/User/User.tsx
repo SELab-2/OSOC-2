83885496b7d0094839b860cd00a9d8c45d576207
Mouwrice <mauricevanwassenhove@gmail.com>
2022-04-03 19:49:45 +0200

    feat: pending button and button skeleton code

--- frontend/components/User/User.tsx
+++ frontend/components/User/User.tsx
@@ -5,7 +5,8 @@ import CoachIconColor from "../../public/images/coach_icon_color.png"
 import CoachIcon from "../../public/images/coach_icon.png";
 import ForbiddenIconColor from "../../public/images/forbidden_icon_color.png"
 import ForbiddenIcon from "../../public/images/forbidden_icon.png"
-import React, {useState} from "react";
+import GreenCheckMark from "../../public/images/green_check_mark.png"
+import React, {SyntheticEvent, useState} from "react";
 import Image from "next/image";
 
 
@@ -21,18 +22,48 @@ export const User: React.FC<{ userName: string, userEmail: string, userIsAdmin: 
 
     const [name] = useState<string>(userName)
     const [email] = useState<string>(userEmail)
-    const [isAdmin] = useState<boolean>(userIsAdmin)
-    const [isCoach] = useState<boolean>(userIsCoach)
-    const [status] = useState<string>(userStatus)
+    const [isAdmin, setIsAdmin] = useState<boolean>(userIsAdmin)
+    const [isCoach, setIsCoach] = useState<boolean>(userIsCoach)
+    const [status, setStatus] = useState<string>(userStatus)
 
-    // TODO -- Create a function for every button
+    const toggleIsAdmin = async (e: SyntheticEvent) => {
+        e.preventDefault()
+        setIsAdmin(!isAdmin)
+        // TODO -- Send the isAdmin value to the backend
+        //      -- If error revert to old value
+    }
+
+    const toggleIsCoach = async (e: SyntheticEvent) => {
+        e.preventDefault()
+        setIsCoach(!isCoach)
+        // TODO -- Send the isCoach value to the backend
+        //      -- If error revert to old value
+    }
+
+    const toggleStatus = async (e: SyntheticEvent) => {
+        e.preventDefault()
+        if (status == 'ACTIVATED') {
+            setStatus('DISABLED')
+        } else {
+            setStatus('ACTIVATED')
+        }
+        // TODO -- Send the status value to the backend
+        //      -- If error revert to old value
+    }
+
+    const activateUser = async (e: SyntheticEvent) => {
+        e.preventDefault()
+        setStatus('ACTIVATED')
+        // TODO -- Send the status value to the backend
+        //      -- If error revert to old value
+    }
 
     return (
         <div className={styles.row}>
             <p>{name}</p>
             <p>{email}</p>
             <div className={styles.buttons}>
-                <div className={styles.buttonContainer}>
+                <div className={styles.buttonContainer} onClick={toggleIsAdmin}>
                     <div className={styles.button}>
                         <Image className={styles.buttonImage} width={30} height={30}
                                src={isAdmin ? AdminIconColor : AdminIcon}
@@ -40,7 +71,7 @@ export const User: React.FC<{ userName: string, userEmail: string, userIsAdmin: 
                     </div>
                     <p>Admin</p>
                 </div>
-                <div className={styles.buttonContainer}>
+                <div className={styles.buttonContainer} onClick={toggleIsCoach}>
                     <div className={styles.button}>
                         <Image className={styles.buttonImage} src={isCoach ? CoachIconColor : CoachIcon} width={30}
                                height={30}
@@ -48,14 +79,24 @@ export const User: React.FC<{ userName: string, userEmail: string, userIsAdmin: 
                     </div>
                     <p>Coach</p>
                 </div>
-                <div className={styles.buttonContainer}>
-                    <div className={styles.button}>
-                        <Image className={styles.buttonImage}
-                               src={status === 'DISABLED' ? ForbiddenIconColor : ForbiddenIcon}
-                               width={30} height={30} alt={"Disabled"}/>
+                {status === 'PENDING' ?
+                    <div className={styles.buttonContainer} onClick={activateUser}>
+                        <div className={styles.button}>
+                            <Image className={styles.buttonImage}
+                                   src={GreenCheckMark}
+                                   width={30} height={30} alt={"Disabled"}/>
+                        </div>
+                        <p style={{fontWeight: 'bold'}}>Pending</p>
                     </div>
-                    <p>Disabled</p>
-                </div>
+                    :
+                    <div className={styles.buttonContainer} onClick={toggleStatus}>
+                        <div className={styles.button}>
+                            <Image className={styles.buttonImage}
+                                   src={status === 'DISABLED' ? ForbiddenIconColor : ForbiddenIcon}
+                                   width={30} height={30} alt={"Disabled"}/>
+                        </div>
+                        <p>Disabled</p>
+                    </div>}
             </div>
         </div>)
 }
