2b3277b893219738a9f4c5c76c4b364bc7acd321
Mouwrice <mauricevanwassenhove@gmail.com>
2022-04-04 19:42:02 +0200

    feat: reset password seems to work

--- frontend/pages/reset/[pid].tsx
+++ frontend/pages/reset/[pid].tsx
@@ -3,6 +3,7 @@ import {useRouter} from "next/router";
 import {Header} from "../../components/Header/Header";
 import styles from "../../styles/login.module.scss";
 import {SyntheticEvent, useEffect, useState} from "react";
+import crypto from "crypto";
 
 /**
  * Landing page for when you click the reset password link from your email
@@ -14,30 +15,32 @@ const Pid: NextPage = () => {
     const router = useRouter()
     const {pid} = router.query // pid is the session key
 
-
-    /**
-     * Check if the provided code is a valid code
-     */
-    useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/reset/${pid}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'applicatin'
-            }
-        }).then(response => {
-            // The code is not valid
-            if (!response.ok) {
-                router.push("/reset").then()
-            }
-        })
-    })
-
     const [newPassword, setNewPassword] = useState<string>("");
     const [newPasswordError, setNewPasswordError] = useState<string>("");
     const [confirmNewPassword, setConfirmNewPassword] = useState<string>("");
     const [confirmNewPasswordError, setConfirmNewPasswordError] = useState<string>("");
     const [backendError] = useState<string>("");
+
+
+    /**
+     * Check if the provided code is a valid code
+     */
+    useEffect(() => {
+        if (pid !== undefined) {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}/reset/${pid}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'applicatin'
+                }
+            }).then(response => {
+                // The code is not valid
+                if (!response.ok) {
+                    router.push("/reset").then()
+                }
+            })
+        }
+    }, [pid, router])
 
     /**
      * Handles all the logic for resetting the password
@@ -64,28 +67,44 @@ const Pid: NextPage = () => {
         }
 
         if (!error) {
-            console.log('RESETTING PASSWORD')
+            // We encrypt the password before sending it to the backend api
+            const encryptedPassword = crypto.createHash('sha256').update(newPassword).digest('hex');
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}/reset/${pid}`, {
+                method: 'POST',
+                body: JSON.stringify({password: encryptedPassword}),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            }).then(response => {
+                if (response.ok) {
+                    router.push("/login").then()
+                }
+            }).catch(error => console.log(error))
         }
     }
 
     return <>
         <Header/>
-        <form className={`${styles.body} ${styles.form}`}>
-            <label className={styles.label}>
-                New Password
-                <input type="password" name="newPassword" value={newPassword}
-                       onChange={e => setNewPassword(e.target.value)}/>
-            </label>
-            <p className={`${styles.textFieldError} ${newPasswordError !== "" ? styles.anim : ""}`}>{newPasswordError}</p>
-            <label className={styles.label}>
-                Confirm New Password
-                <input type="password" name="confirmNewPassword" value={confirmNewPassword}
-                       onChange={e => setConfirmNewPassword(e.target.value)}/>
-            </label>
-            <p className={`${styles.textFieldError} ${confirmNewPasswordError !== "" ? styles.anim : ""}`}>{confirmNewPasswordError}</p>
-            <button onClick={e => resetPassword(e)}>RESET PASSWORD</button>
-            <p className={`${styles.textFieldError} ${backendError !== "" ? styles.anim : ""}`}>{backendError}</p>
-        </form>
+        <div className={styles.body}>
+            <h2>Reset Password</h2>
+            <form className={styles.form}>
+                <label className={styles.label}>
+                    New Password
+                    <input type="password" name="newPassword" value={newPassword}
+                           onChange={e => setNewPassword(e.target.value)}/>
+                </label>
+                <p className={`${styles.textFieldError} ${newPasswordError !== "" ? styles.anim : ""}`}>{newPasswordError}</p>
+                <label className={styles.label}>
+                    Confirm New Password
+                    <input type="password" name="confirmNewPassword" value={confirmNewPassword}
+                           onChange={e => setConfirmNewPassword(e.target.value)}/>
+                </label>
+                <p className={`${styles.textFieldError} ${confirmNewPasswordError !== "" ? styles.anim : ""}`}>{confirmNewPasswordError}</p>
+                <button onClick={e => resetPassword(e)}>RESET PASSWORD</button>
+                <p className={`${styles.textFieldError} ${backendError !== "" ? styles.anim : ""}`}>{backendError}</p>
+            </form>
+        </div>
     </>
 }
 
