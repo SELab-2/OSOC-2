7ed8d2e664a4410e0d025709c195518ffa235189
Mouwrice <mauricevanwassenhove@gmail.com>
2022-04-06 18:17:15 +0200

    feat: add a footer with a link to the docs

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
 import { SessionProvider } from "../contexts/sessionProvider";
 import {Header} from "../components/Header/Header";
+import {Footer} from "../components/Footer/Footer";
 
 function App({Component, pageProps}: AppProps) {
 
@@ -9,6 +10,7 @@ function App({Component, pageProps}: AppProps) {
         <SessionProvider>
             <Header/>
             <Component {...pageProps} />
+            <Footer />
         </SessionProvider>
 
     )
