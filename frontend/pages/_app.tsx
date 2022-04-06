5f523f1f9bfd44a61f7bb27f98ec0c0bc4080ae0
Mouwrice <mauricevanwassenhove@gmail.com>
2022-04-06 18:01:18 +0200

    feat: moved the header to _app to be permanent

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
 import { SessionProvider } from "../contexts/sessionProvider";
+import {Header} from "../components/Header/Header";
 
 function App({Component, pageProps}: AppProps) {
 
     return (
         <SessionProvider>
+            <Header/>
             <Component {...pageProps} />
         </SessionProvider>
 
