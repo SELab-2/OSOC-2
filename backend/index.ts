5fcd340874a9f7ba925d18a5e09fc968a195ec1a
Bram Devlaminck <bram.devlaminck@gmail.com>
2022-04-08 10:53:51 +0200

    feat: use iosocket as app

--- backend/index.ts
+++ backend/index.ts
@@ -5,9 +5,13 @@ import * as config from './config.json';
 import * as ep from './endpoints'
 import * as util from './utility';
 import cors from 'cors';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app: express.Application = express();
 const port: number = config.port;
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
 app.use(body.urlencoded({extended: true}));
 app.use(express.json());
@@ -16,8 +20,10 @@ app.use((req, _, next) => util.logRequest(req, next));
 ep.attach(app);
 config.global.homes.forEach(home => util.addInvalidVerbs(app, home + "/"));
 
-// Server setup
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("websocket connection achieved " + JSON.stringify(socket));
+});
+httpServer.listen(port, () => {
     console.log(`TypeScript with Express
-         http://localhost:${port}/`);
+          http://localhost:${port}/`);
 });
