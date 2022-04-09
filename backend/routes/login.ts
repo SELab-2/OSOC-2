7a609fd7e7506ce8f857c8cdc13e333237a448cf
Mouwrice <mauricevanwassenhove@gmail.com>
2022-04-09 11:08:38 +0200

    fix: login.ts did not consistently use 4 space tabs

--- backend/routes/login.ts
+++ backend/routes/login.ts
@@ -1,16 +1,13 @@
 import express from 'express';
 
 import {getPasswordPersonByEmail} from '../orm_functions/person';
-import {
-  addSessionKey,
-  removeAllKeysForUser
-} from '../orm_functions/session_key';
+import {addSessionKey, removeAllKeysForUser} from '../orm_functions/session_key';
 import {parseLoginRequest, parseLogoutRequest} from '../request';
 import {Responses} from '../types';
 import * as util from '../utility';
 
-function orDefault<T>(v: T|undefined, def: T): T {
-  return (v == undefined || v == null) ? def : v;
+function orDefault<T>(v: T | undefined, def: T): T {
+    return (v == undefined || false) ? def : v;
 }
 
 /**
@@ -20,27 +17,27 @@ function orDefault<T>(v: T|undefined, def: T): T {
  * `Promise.resolve`, failures using `Promise.reject`.
  */
 async function login(req: express.Request): Promise<Responses.Login> {
-  console.log("Calling login endpoint " + JSON.stringify(req.body));
-  return parseLoginRequest(req).then(
-      // TODO normalize email
-      parsed => getPasswordPersonByEmail(parsed.name).then(async pass => {
-        if (pass == null || pass.login_user == null ||
-            pass?.login_user?.password != parsed.pass) {
-          return Promise.reject(
-              {http : 409, reason : 'Invalid e-mail or password.'});
-        }
-        if (pass?.login_user?.account_status != 'ACTIVATED') {
-          return Promise.reject(
-              {http : 409, reason : 'Account isn\'t activated yet.'});
-        }
-        const key: string = util.generateKey();
-        return addSessionKey(pass.login_user.login_user_id, key)
-            .then(ins => ({
-                    sessionkey : ins.session_key,
-                    is_admin : orDefault(pass?.login_user?.is_admin, false),
-                    is_coach : orDefault(pass?.login_user?.is_coach, false)
-                  }));
-      }));
+    console.log("Calling login endpoint " + JSON.stringify(req.body));
+    return parseLoginRequest(req).then(
+        // TODO normalize email
+        parsed => getPasswordPersonByEmail(parsed.name).then(async pass => {
+            if (pass == null || pass.login_user == null ||
+                pass?.login_user?.password != parsed.pass) {
+                return Promise.reject(
+                    {http: 409, reason: 'Invalid e-mail or password.'});
+            }
+            if (pass?.login_user?.account_status != 'ACTIVATED') {
+                return Promise.reject(
+                    {http: 409, reason: 'Account isn\'t activated yet.'});
+            }
+            const key: string = util.generateKey();
+            return addSessionKey(pass.login_user.login_user_id, key)
+                .then(ins => ({
+                    sessionkey: ins.session_key,
+                    is_admin: orDefault(pass?.login_user?.is_admin, false),
+                    is_coach: orDefault(pass?.login_user?.is_coach, false)
+                }));
+        }));
 }
 
 /**
@@ -50,12 +47,14 @@ async function login(req: express.Request): Promise<Responses.Login> {
  * `Promise.resolve`, failures using `Promise.reject`.
  */
 async function logout(req: express.Request): Promise<Responses.Empty> {
-  return parseLogoutRequest(req)
-      .then(parsed => util.checkSessionKey(parsed))
-      .then(async checked => {
-        return removeAllKeysForUser(checked.data.sessionkey)
-            .then(() => { return Promise.resolve({}); });
-      })
+    return parseLogoutRequest(req)
+        .then(parsed => util.checkSessionKey(parsed))
+        .then(async checked => {
+            return removeAllKeysForUser(checked.data.sessionkey)
+                .then(() => {
+                    return Promise.resolve({});
+                });
+        })
 }
 
 /**
@@ -64,11 +63,11 @@ async function logout(req: express.Request): Promise<Responses.Empty> {
  * endpoints.
  */
 export function getRouter(): express.Router {
-  const router: express.Router = express.Router();
+    const router: express.Router = express.Router();
 
-  util.routeKeyOnly(router, 'post', '/', login);
-  router.delete('/',
-                (req, res) => util.respOrErrorNoReinject(res, logout(req)));
-  util.addInvalidVerbs(router, '/');
-  return router;
+    util.routeKeyOnly(router, 'post', '/', login);
+    router.delete('/',
+        (req, res) => util.respOrErrorNoReinject(res, logout(req)));
+    util.addInvalidVerbs(router, '/');
+    return router;
 }
