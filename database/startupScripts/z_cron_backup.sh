c9c4a22327ad17722e888ec499e80f86727493c4
KoenDesplenter <koen.desplenter@telenet.be>
2022-04-14 10:50:44 +0200

    fix: removed cron from docker

--- database/startupScripts/z_cron_backup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-whoami
-echo "running backup cron"
-service cron status
-service cron start
---
+++ database_backup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+apt-get -y install cron
+
+# Commands for backup:
+# Make Backup folder 
+mkdir /home/backups
+
+# Place cron job for backup in cron file
+echo "* * * * * docker exec -t osoc-2-db-1 pg_dump -U osoc2 osoc2 > /home/backups/osoc2-$(date +%d).bak\n# An empty line is required at the end of this file for a valid cron file." >> /etc/cron.d/backup
+
+# Give execution rights on the cron job
+chmod 0644 /etc/cron.d/backup
+
+# Apply cron job
+crontab /etc/cron.d/backup
+service cron start
+service cron status
