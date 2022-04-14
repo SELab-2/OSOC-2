f16f259c07f74029d7fd516fbe40c62689ebcb31
KoenDesplenter <koen.desplenter@telenet.be>
2022-04-14 11:18:43 +0200

    feat: cron job database backup

--- database_backup.sh
+++ database_backup.sh
@@ -6,7 +6,7 @@ apt-get -y install cron
 mkdir /home/backups
 
 # Place cron job for backup in cron file
-echo "* * * * * docker exec -t osoc-2-db-1 pg_dump -U osoc2 osoc2 > /home/backups/osoc2-$(date +%d).bak\n# An empty line is required at the end of this file for a valid cron file." >> /etc/cron.d/backup
+printf "30 0 * * * root docker exec -t osoc-2-db-1 pg_dump -U osoc2 osoc2 > /home/backups/osoc2-$(date +%d).bak\n# An empty line is required at the end of this file for a valid cron file." > /etc/cron.d/backup
 
 # Give execution rights on the cron job
 chmod 0644 /etc/cron.d/backup
