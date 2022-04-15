e17db3f6f798cf73a44acf499302b266145ce60a
Bram Devlaminck <bram.devlaminck@gmail.com>
2022-04-15 17:05:06 +0200

    fix: add typing for children in socketsProvider

--- frontend/contexts/socketProvider.tsx
+++ frontend/contexts/socketProvider.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, useContext} from "react";
+import React, {createContext, ReactNode, useContext} from "react";
 import {io} from "socket.io-client";
 import {Socket} from "socket.io-client";
 import {ClientToServerEvents, ServerToClientEvents} from "../types";
@@ -10,7 +10,7 @@ const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(`${process
 const SocketsContext = createContext({socket});
 
 // provider for the socketcontext
-export const SocketsProvider: React.FC = ({ children }) => {
+export const SocketsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     return (
         <SocketsContext.Provider value={{socket}}>
             {children}
