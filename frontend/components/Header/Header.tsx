6b6635449fa99d3c0829f8baf6f36610cbfa9a60
KoenDesplenter <koen.desplenter@telenet.be>
2022-04-23 13:34:46 +0200

    feat: created osoc edition page

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -65,6 +65,9 @@ export const Header: React.FC = () => {
                 }`}
             >
                 {sessionKey !== "" && !router.pathname.startsWith("/reset") ? (
+                    <Link href={"/osoc_editions"}>Osoc Editions</Link>
+                ) : null}
+                {sessionKey !== "" && !router.pathname.startsWith("/reset") ? (
                     <Link href={"/students"}>Students</Link>
                 ) : null}
                 {sessionKey !== "" && !router.pathname.startsWith("/reset") ? (
---
+++ frontend/pages/osoc_editions.tsx
@@ -0,0 +1,16 @@
+/**
+ * Projects overview page
+ * TODO
+ */
+import { NextPage } from "next";
+import { UnderConstruction } from "../components/UnderConstruction/UnderConstruction";
+
+const Osoc_editions: NextPage = () => {
+    return (
+        <div>
+            <UnderConstruction />
+        </div>
+    );
+};
+
+export default Osoc_editions;
