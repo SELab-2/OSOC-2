d70f1ff1b6dc5d99e01e62a2d4e8d5fa1f5d4f8a
Huan <huanyutan1999@gmail.com>
2022-04-25 21:30:21 +0200

    feat: first "test"

---
+++ frontend/__tests__/LoginScreen.test.tsx
@@ -0,0 +1,11 @@
+import { User } from "../components/User/User";
+
+describe("User component tests", () => {
+    let container: HTMLDivElement;
+
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.append(container);
+    });
+});
+console.log(User);
