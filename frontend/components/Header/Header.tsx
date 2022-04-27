1db80e21a8dbfffb78520947b5f1557f0a74f04c
Bram Devlaminck <bram.devlaminck@gmail.com>
2022-04-27 16:21:48 +0200

    fix: only 1 request

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -29,7 +29,8 @@ export const Header: React.FC = () => {
                 }
             });
         }
-    }, [getSessionKey]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     const logIn = (e: SyntheticEvent) => {
         e.preventDefault();
