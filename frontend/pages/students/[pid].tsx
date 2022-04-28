51d4c5553d07467560c21b9f2e49f3045d5efdb2
Mouwrice <mauricevanwassenhove@gmail.com>
2022-04-28 21:07:47 +0200

    fix: wrong function argument

--- frontend/pages/students/[pid].tsx
+++ frontend/pages/students/[pid].tsx
@@ -43,12 +43,7 @@ const Pid: NextPage = () => {
     return (
         <div className={styles.studentPage}>
             {student !== undefined ? (
-                <StudentOverview
-                    student={student}
-                    updateEvaluation={() => {
-                        return;
-                    }}
-                />
+                <StudentOverview student={student} />
             ) : null}
         </div>
     );
