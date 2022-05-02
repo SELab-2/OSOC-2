d5239a23184104eab60791e684f1fd5b25fc27c2
Jonathan Vanbrabant <jonathan.vanbrabant@yahoo.com>
2022-05-02 20:59:51 +0200

    feat: students toegevoegd op project screen

--- frontend/components/ProjectCard/ProjectCard.tsx
+++ frontend/components/ProjectCard/ProjectCard.tsx
@@ -3,6 +3,9 @@ import { Project } from "../../types";
 import styles from "./ProjectCard.module.css";
 
 export const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
-    console.log(project);
-    return <div className={styles.card}>TEST</div>;
+    return (
+        <div className={styles.card}>
+            <h2>{project.name}</h2>
+        </div>
+    );
 };
--- frontend/pages/projects.tsx
+++ frontend/pages/projects.tsx
@@ -1,12 +1,15 @@
 import { NextPage } from "next";
 import { useContext, useEffect, useState } from "react";
 import SessionContext from "../contexts/sessionProvider";
-import { Project } from "../types";
+import { Project, Student, Display } from "../types";
 import { ProjectCard } from "../components/ProjectCard/ProjectCard";
+import { StudentCard } from "../components/StudentCard/StudentCard";
+import styles from "../styles/students.module.scss";
 
 const Projects: NextPage = () => {
     const { getSession } = useContext(SessionContext);
     const [projects, setProjects] = useState<Project[]>([]);
+    const [students, setStudents] = useState<Student[]>([]);
 
     const fetchProjects = async () => {
         if (getSession != undefined) {
@@ -29,21 +32,56 @@ const Projects: NextPage = () => {
         }
     };
 
-    const setProjectsHTML = () => {
-        const array = [];
-        for (let i = 0; i < projects.length; i++) {
-            const m = <ProjectCard project={projects[i]} />;
-            array.push(m);
+    const fetchStudents = async () => {
+        if (getSession != undefined) {
+            getSession().then(async ({ sessionKey }) => {
+                const response = await fetch(
+                    `${process.env.NEXT_PUBLIC_API_URL}/student/all`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Authorization: `auth/osoc2 ${sessionKey}`,
+                        },
+                    }
+                )
+                    .then((response) => response.json())
+                    .catch((error) => console.log(error));
+                if (response !== undefined && response.success) {
+                    setStudents(response.data);
+                }
+            });
         }
-        return array;
     };
+
     useEffect(() => {
         fetchProjects().then();
+        fetchStudents().then();
         // We do not want to reload the data when the data changes
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    return <div>{setProjectsHTML()}</div>;
+    return (
+        <div className={`${styles.students} ${styles.limited}`}>
+            <div className={`${styles.students} ${styles.limited}`}>
+                {students.map((student) => {
+                    return (
+                        <div
+                            key={student.student.student_id}
+                            className={styles.card}
+                        >
+                            <StudentCard
+                                student={student}
+                                display={Display.LIMITED}
+                            />
+                        </div>
+                    );
+                })}
+                {projects.map((project) => {
+                    return <ProjectCard key={project.id} project={project} />;
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default Projects;
