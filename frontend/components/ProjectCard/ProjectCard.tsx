2dd660b4ce9b1a2010159c512e3f825d01d669a7
Jonathan Vanbrabant <jonathan.vanbrabant@yahoo.com>
2022-05-03 16:18:38 +0200

    feat: added info to projectcard

--- frontend/components/ProjectCard/ProjectCard.tsx
+++ frontend/components/ProjectCard/ProjectCard.tsx
@@ -6,6 +6,16 @@ export const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
     return (
         <div className={styles.card}>
             <h2>{project.name}</h2>
+            <p>{project.partner}</p>
+            <br></br>
+            <h1>Coaches</h1>
+            {project.coaches.map((coach) => {
+                return (
+                    <p key={coach.login_user.login_user_id}>
+                        {coach.login_user.person.firstname}
+                    </p>
+                );
+            })}
         </div>
     );
 };
