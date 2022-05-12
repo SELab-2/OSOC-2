e85b538e1fae57c0e567eae87a6b3ff865704dc5
Jonathan Vanbrabant <jonathan.vanbrabant@yahoo.com>
2022-05-12 15:56:31 +0200

    feat: basic functionality to create a project

--- frontend/pages/projects/create.tsx
+++ frontend/pages/projects/create.tsx
@@ -1,21 +1,37 @@
 import { NextPage } from "next";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, SyntheticEvent } from "react";
 import SessionContext from "../../contexts/sessionProvider";
-import { Role } from "../../types";
+import { OsocEdition } from "../../types";
+import { Modal } from "../../components/Modal/Modal";
 
 const Create: NextPage = () => {
     const { getSession } = useContext(SessionContext);
 
-    const [roles, setRoles] = useState<Role[]>([]);
+    const formatDate = () => {
+        const date = new Date();
+        return [
+            date.getFullYear(),
+            padTo2Digits(date.getMonth() + 1),
+            padTo2Digits(date.getDate()),
+        ].join("-");
+    };
+
+    const padTo2Digits = (num: number) => {
+        return num.toString().padStart(2, "0");
+    };
+
     const [projectName, setProjectName] = useState<string>("");
     const [partner, setPartner] = useState<string>("");
     const [osocEdition, setOsocEdition] = useState<string>("");
-    const [startDate, setStartDate] = useState<string>(Date());
-    const [endDate, setEndDate] = useState<string>(Date());
-    const [positions, setPositions] = useState<string>("0");
+    const [startDate, setStartDate] = useState<string>(formatDate());
+    const [endDate, setEndDate] = useState<string>(formatDate());
     const [rolePositions, setRolePositions] = useState<{ [K: string]: string }>(
         {}
     );
+    const [visible, setVisible] = useState<boolean>(false);
+    const [newRoleName, setNewRoleName] = useState<string>("");
+    const [newRolePositions, setNewRolePositions] = useState<string>("0");
+    const [osocs, setOsocs] = useState<OsocEdition[]>([]);
 
     const fetchRoles = async () => {
         if (getSession != undefined) {
@@ -32,7 +48,32 @@ const Create: NextPage = () => {
                     .then((response) => response.json())
                     .catch((error) => console.log(error));
                 if (response !== undefined && response.success) {
-                    setRoles(response.data);
+                    const roleMap: { [K: string]: string } = {};
+                    for (const role of response.data) {
+                        roleMap[role.name] = "0";
+                    }
+                    setRolePositions(roleMap);
+                }
+            });
+        }
+    };
+
+    const fetchOsocs = async () => {
+        if (getSession != undefined) {
+            getSession().then(async ({ sessionKey }) => {
+                const response = await fetch(
+                    `${process.env.NEXT_PUBLIC_API_URL}/osoc/all`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Authorization: `auth/osoc2 ${sessionKey}`,
+                        },
+                    }
+                )
+                    .then((response) => response.json())
+                    .catch((error) => console.log(error));
+                if (response !== undefined && response.success) {
+                    setOsocs(response.data);
                 }
             });
         }
@@ -40,15 +81,145 @@ const Create: NextPage = () => {
 
     useEffect(() => {
         fetchRoles().then();
+        fetchOsocs().then();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const handleConfirm = () => {
-        console.log("click");
+        if (getSession !== undefined) {
+            getSession().then(async ({ sessionKey }) => {
+                if (sessionKey != "") {
+                    const response = await fetch(
+                        `${process.env.NEXT_PUBLIC_API_URL}/project`,
+                        {
+                            method: "POST",
+                            headers: {
+                                Authorization: `auth/osoc2 ${sessionKey}`,
+                                "Content-Type": "application/json",
+                                Accept: "application/json",
+                            },
+                            body: JSON.stringify({
+                                name: projectName,
+                                partner: partner,
+                                start: startDate,
+                                end: endDate,
+                                osocId: getOsocId(osocEdition),
+                                positions: getTotalPositions(),
+                                roles: { roles: getRoleList() },
+                            }),
+                        }
+                    )
+                        .then((response) => response.json())
+                        .catch((error) => console.log(error));
+                    if (response !== undefined && response.success) {
+                        alert("Project succesfully created!");
+                        window.close();
+                    }
+                }
+            });
+        }
+    };
+
+    const getOsocId = (year: string) => {
+        for (const osoc of osocs) {
+            if (osoc.year === parseInt(year)) {
+                return osoc.osoc_id;
+            }
+        }
+    };
+
+    const getTotalPositions = () => {
+        let total = 0;
+        for (const key in rolePositions) {
+            total += parseInt(rolePositions[key]);
+        }
+        return total;
+    };
+
+    const getRoleList = () => {
+        const roleList = [];
+        for (const role in rolePositions) {
+            if (parseInt(rolePositions[role]) > 0) {
+                const obj = {
+                    name: role,
+                    positions: parseInt(rolePositions[role]),
+                };
+                roleList.push(obj);
+            }
+        }
+        return roleList;
+    };
+
+    const addRole = (name: string, positions: string) => {
+        const newRole: { [k: string]: string } = {};
+        newRole[name] = positions;
+        setRolePositions((rolePositions) => ({ ...rolePositions, ...newRole }));
+    };
+
+    const closer = (e: SyntheticEvent) => {
+        e.preventDefault();
+        setNewRoleName("");
+        setNewRolePositions("0");
+        setVisible(false);
+    };
+
+    const addNewRole = () => {
+        if (getSession !== undefined) {
+            getSession().then(async ({ sessionKey }) => {
+                if (sessionKey != "") {
+                    const response = await fetch(
+                        `${process.env.NEXT_PUBLIC_API_URL}/role/create`,
+                        {
+                            method: "POST",
+                            headers: {
+                                Authorization: `auth/osoc2 ${sessionKey}`,
+                                "Content-Type": "application/json",
+                                Accept: "application/json",
+                            },
+                            body: JSON.stringify({
+                                name: newRoleName,
+                            }),
+                        }
+                    )
+                        .then((response) => response.json())
+                        .catch((error) => console.log(error));
+                    if (response !== undefined && response.success) {
+                        setVisible(false);
+                        addRole(response.name, newRolePositions);
+                        setNewRoleName("");
+                        setNewRolePositions("0");
+                    }
+                }
+            });
+        }
     };
 
     return (
         <div>
+            <Modal
+                title="Type in the role name and the number of positions"
+                visible={visible}
+                handleClose={closer}
+            >
+                <label>Role: </label>
+                <input
+                    type="text"
+                    name="role_name"
+                    placeholder="name..."
+                    value={newRoleName}
+                    onChange={(e) => setNewRoleName(e.target.value)}
+                />
+                <br />
+                <label>Positions: </label>
+                <input
+                    type="number"
+                    name="role_positions"
+                    value={newRolePositions}
+                    onChange={(e) => setNewRolePositions(e.target.value)}
+                />
+                <br />
+                <button onClick={() => addNewRole()}>Add Role</button>
+            </Modal>
             <p>Project name</p>
             <input
                 type="text"
@@ -87,41 +258,23 @@ const Create: NextPage = () => {
                 value={endDate}
                 onChange={(e) => setEndDate(e.target.value)}
             />
-            <p>Total Students Required</p>
-            <input
-                type="number"
-                name="positions"
-                min="0"
-                value={positions}
-                onChange={(e) => setPositions(e.target.value)}
-            />
-            <p>Pick the necessary roles</p>
-            {roles.map((role) => {
+            <p>Change the amount of positions for the necessary roles:</p>
+            {Object.keys(rolePositions).map((role) => {
                 return (
-                    <div key={role.role_id}>
-                        <input type="checkbox" id={role.name} />
-                        <label> {role.name} </label>
+                    <div key={role}>
                         <div>
+                            <label> {role}: </label>
                             <input
                                 type="number"
-                                min="0"
-                                value={
-                                    parseInt(rolePositions[role.name]) || "0"
-                                }
-                                onChange={(e) => {
-                                    const map = rolePositions;
-                                    map[role.name] = e.target.value;
-                                    setRolePositions(map);
-                                }}
+                                value={rolePositions[role]}
+                                onChange={(e) => addRole(role, e.target.value)}
                             />
-                            <label> Add number of positions</label>
                         </div>
-                        <br />
                     </div>
                 );
             })}
-            <p>Or create a new role</p>
-            <input type="text" name="osoc_edition" placeholder="year..." />
+            <a onClick={() => setVisible(true)}>Create a new role</a>
+            <br />
             <button onClick={handleConfirm}>CONFIRM</button>
         </div>
     );
