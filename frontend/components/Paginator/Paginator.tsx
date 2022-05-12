309e9e5121872b146651d707c3d552a19b0871d2
Mouwrice <mauricevanwassenhove@gmail.com>
2022-05-12 15:31:58 +0200

    feat: add previous and next page buttons

--- frontend/components/Paginator/Paginator.tsx
+++ frontend/components/Paginator/Paginator.tsx
@@ -1,12 +1,13 @@
 import { pageSize, Pagination } from "../../types";
 import React from "react";
+import styles from "./Paginator.module.scss";
 
 export const Paginator: React.FC<{
     pagination: Pagination;
     navigator: (page: number) => void;
 }> = ({ pagination, navigator }) => {
     const paginator = [];
-    const pages = pagination.count / pageSize;
+    const pages = Math.ceil(pagination.count / pageSize);
     for (let i = 0; i < pages; i++) {
         paginator.push(
             <li key={i}>
@@ -22,9 +23,42 @@ export const Paginator: React.FC<{
         );
     }
 
+    const previousPage = () => {
+        if (pagination.page === 0) return;
+        navigator(pagination.page - 1);
+    };
+
+    const nextPage = () => {
+        if (pagination.page === pages - 1) return;
+        navigator(pagination.page + 1);
+    };
+
     return (
-        <nav className="pagination" role="navigation" aria-label="pagination">
+        <nav
+            className={`is-centered pagination ${styles.pagination}`}
+            role="navigation"
+            aria-label="pagination"
+        >
+            <a
+                onClick={previousPage}
+                className={`pagination-previous ${
+                    pagination.page === 0 ? "is-disabled" : ""
+                }`}
+                title="This is the first page"
+            >
+                Previous
+            </a>
             <ul className="pagination-list">{paginator}</ul>
+            <a
+                onClick={nextPage}
+                className={`pagination-next ${
+                    pagination.page === pages - 1 || pages === 0
+                        ? "is-disabled"
+                        : ""
+                }`}
+            >
+                Next page
+            </a>
         </nav>
     );
 };
