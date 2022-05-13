677bee2460a67ef73d815aaef67a7aaa5126c6bd
Norick Beterams <norick.beterams@ugent.be>
2022-05-14 00:03:02 +0200

    test: most fluent language tests

--- backend/tests/routes_unit/form/most_fluent_language_files/most_fluent_language.test.ts
+++ backend/tests/routes_unit/form/most_fluent_language_files/most_fluent_language.test.ts
@@ -3,6 +3,30 @@ import { getMostFluentLanguage, readFile } from "../../../../routes/form";
 import { Requests } from "../../../../types";
 import Form = Requests.Form;
 
+test("Most fluent language question absent", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/most_fluent_language_files",
+        "mostFluentLanguageQuestionAbsent.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getMostFluentLanguage(data as Form)).rejects.toBe(
+        errors.cookArgumentError()
+    );
+});
+
+test("Most fluent language options absent", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/most_fluent_language_files",
+        "mostFluentLanguageOptionsAbsent.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getMostFluentLanguage(data as Form)).rejects.toBe(
+        errors.cookArgumentError()
+    );
+});
+
 test("Most fluent language other option question absent", async () => {
     const data = await readFile(
         "../tests/routes_unit/form/most_fluent_language_files",
