e4a819e43aab12577ad5b6dfea6df56cb6d61515
Norick Beterams <norick.beterams@ugent.be>
2022-05-13 23:56:53 +0200

    test: education level tests

---
+++ backend/tests/routes_unit/form/education_level_files/education_level.test.ts
@@ -0,0 +1,52 @@
+import { errors } from "../../../../utility";
+import { getEducationLevel, readFile } from "../../../../routes/form";
+import { Requests } from "../../../../types";
+import Form = Requests.Form;
+
+test("Education level question absent", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/education_level_files",
+        "educationLevelQuestionAbsent.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getEducationLevel(data as Form)).rejects.toBe(
+        errors.cookArgumentError()
+    );
+});
+
+test("Education level options with the same id", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/education_level_files",
+        "educationLevelOptionsWithSameId.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getEducationLevel(data as Form)).rejects.toBe(
+        errors.cookArgumentError()
+    );
+});
+
+test("Education level other question absent", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/education_level_files",
+        "educationLevelOtherQuestionAbsent.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getEducationLevel(data as Form)).rejects.toBe(
+        errors.cookArgumentError()
+    );
+});
+
+test("Education level other question value not null", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/education_level_files",
+        "educationLevelOtherQuestionNotNull.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getEducationLevel(data as Form)).resolves.toStrictEqual(
+        "No diploma"
+    );
+});
