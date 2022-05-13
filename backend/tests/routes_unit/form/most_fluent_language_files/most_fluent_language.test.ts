601a5438f02f1cbdf017f104aefc47e4afa637f6
Norick Beterams <norick.beterams@ugent.be>
2022-05-13 23:30:42 +0200

    test: most fluent language tests

---
+++ backend/tests/routes_unit/form/most_fluent_language_files/most_fluent_language.test.ts
@@ -0,0 +1,40 @@
+import { errors } from "../../../../utility";
+import { getMostFluentLanguage, readFile } from "../../../../routes/form";
+import { Requests } from "../../../../types";
+import Form = Requests.Form;
+
+test("Most fluent language other option question absent", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/most_fluent_language_files",
+        "otherQuestionAbsent.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getMostFluentLanguage(data as Form)).rejects.toBe(
+        errors.cookArgumentError()
+    );
+});
+
+test("Most fluent language other option question not absent", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/most_fluent_language_files",
+        "otherQuestionNotAbsent.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getMostFluentLanguage(data as Form)).resolves.toStrictEqual(
+        "Danish"
+    );
+});
+
+test("Most fluent language value is not other", async () => {
+    const data = await readFile(
+        "../tests/routes_unit/form/most_fluent_language_files",
+        "mostFluentLanguageNotOther.json"
+    );
+    expect(data).not.toBeNull();
+
+    await expect(getMostFluentLanguage(data as Form)).resolves.toStrictEqual(
+        "Dutch"
+    );
+});
