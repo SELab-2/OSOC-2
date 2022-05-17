1f0c3b4642f19b9893d55bb2e4673372d1e00f80
KoenDesplenter <koen.desplenter@telenet.be>
2022-05-17 18:28:13 +0200

    feat: backend changes

--- backend/tests/request.test.ts
+++ backend/tests/request.test.ts
@@ -541,7 +541,7 @@ test("Can parse filter students request", () => {
         statusFilter: "YES",
     };
     const emailStatusFilter: T.Requests.StudentFilterParameters = {
-        emailStatusFilter: "SENT",
+        emailStatusFilter: "APPROVED",
     };
     const nameSort: T.Requests.StudentFilterParameters = {
         nameSort: "asc",
